1961437c44fd0cb654f702e40052e4ae59822b4c
johan.martinsson <johan.martinsson@dexyos.fr>
2022-05-24 01:05:21 +0200

    make all tests pass. start the app with no votes

--- backend/src/language-leaderboard.spec.ts
+++ backend/src/language-leaderboard.spec.ts
@@ -2,6 +2,13 @@ import request from "supertest"
 import {expect} from "chai"
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import {startApp} from "./language-leaderboard"
+import {Express} from "express"
+
+async function setInitialVotes(app: Express, votes: any) {
+    await request(app).put('/admin/configureVotes')
+        .send(votes)
+        .expect(200)
+}
 
 describe('LanguageLeaderBoard', () => {
 
@@ -9,9 +16,6 @@ describe('LanguageLeaderBoard', () => {
     let mongod: MongoMemoryServer
     beforeEach(async () => {
         mongod = await MongoMemoryServer.create()
-
-
-
     });
 
     afterEach(async () => {
@@ -20,9 +24,7 @@ describe('LanguageLeaderBoard', () => {
 
     it('returns existing languages with votes', async () => {
         const app = await startApp(mongod)
-        await request(app).put('/admin/configureVotes')
-            .send([{name: "java", votes: 3}, {name: "js", votes:15}])
-            .expect(200)
+        await setInitialVotes(app, [{name: "java", votes: 3}, {name: "js", votes: 15}])
         const result = await request(app).get('/').expect(200)
         expect(result.body).to.deep.equal({
             languages: [{name: "java", votes: 3}, {name: "js", votes:15}]
@@ -31,6 +33,7 @@ describe('LanguageLeaderBoard', () => {
 
     it('should accept votes', async () => {
         const app = await startApp(mongod)
+        await setInitialVotes(app, [{name: "java", votes: 3}, {name: "js", votes: 15}])
 
         const voteResult = await request(app).put('/vote/java').expect(200)
         expect(voteResult.body).to.deep.equal({message: "a voté pour java"})
@@ -43,6 +46,7 @@ describe('LanguageLeaderBoard', () => {
 
     it('should persist votes', async () => {
         const app1 = await startApp(mongod)
+        await setInitialVotes(app1, [{name: "java", votes: 3}, {name: "js", votes: 15}])
 
         const voteResult = await request(app1).put('/vote/java').expect(200)
         expect(voteResult.body).to.deep.equal({message: "a voté pour java"})
