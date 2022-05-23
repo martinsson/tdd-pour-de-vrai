99a31e958eb425c708d7e1bec770d284eb97d2e3
johan.martinsson <johan.martinsson@dexyos.fr>
2022-05-24 01:02:20 +0200

    configured wallaby

---
+++ backend/wallaby.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+    return {
+        files: [
+            'src/**/*.ts', // adjust if required
+            '!src/**/*.spec.ts'
+        ],
+
+        tests: [
+            'src/**/*.spec.ts' // adjust if required
+        ],
+
+        env: {
+            type: 'node'
+        }
+    };
+};
