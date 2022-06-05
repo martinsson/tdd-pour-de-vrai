45332375a767f06060d7dec82a49dec8f37d19a2
Xavier NOPRE <xavier.nopre@vrtice.com>
2022-06-05 15:50:55 +0200

    FIX arrête de l'app pour les TU (erreur jest)

--- backend/src/language-leaderboard.ts
+++ backend/src/language-leaderboard.ts
@@ -1,53 +1,57 @@
-import express from "express"
-import mysql from "mysql2/promise"
-import {MongoMemoryServer} from "mongodb-memory-server"
+import express from "express";
+import { MongoMemoryServer } from "mongodb-memory-server";
 import * as mongoDB from "mongodb";
+import { MongoClient } from "mongodb";
 
 class LanguageVote {
-    constructor(public readonly name: string, public readonly votes: number) {
+  constructor(public readonly name: string, public readonly votes: number) {}
+}
 
+let client: MongoClient;
+
+export async function startApp(mongod: MongoMemoryServer) {
+  const app = express();
+
+  app.use(express.json());
+  client = new mongoDB.MongoClient(mongod.getUri());
+  await client.connect();
+  const db = client.db("real-world-tdd");
+  const votesCollection = db.collection("votes");
+
+  app.put("/admin/configureVotes", async (req, res) => {
+    try {
+      await votesCollection.drop();
+    } catch (e) {
+      console.warn("here");
     }
 
+    await votesCollection.insertMany(req.body);
 
+    res.send({});
+  });
+
+  app.get("/", async (req, res) => {
+    const languagesAndVotes = (await votesCollection.find().toArray()).map(
+      ({ name, votes }) => new LanguageVote(name, votes)
+    );
+    console.log(languagesAndVotes);
+    res.send({
+      languages: languagesAndVotes,
+    });
+  });
+
+  app.put("/vote/:langName", async (req, res) => {
+    const languagename = req.params["langName"];
+    await votesCollection.updateOne(
+      { name: languagename },
+      { $inc: { votes: 1 } }
+    );
+    res.send({ message: "a voté pour " + languagename });
+  });
+
+  return app;
 }
-export async function startApp(mongod: MongoMemoryServer) {
-    const app = express()
 
-    app.use(express.json())
-    const client = new mongoDB.MongoClient(mongod.getUri())
-    await client.connect()
-    const db = client.db('real-world-tdd')
-    const votesCollection = db.collection('votes')
-
-
-    app.put('/admin/configureVotes', async (req, res) => {
-        try {
-            await votesCollection.drop()
-        } catch (e) {
-            console.warn("here")
-        }
-
-        await votesCollection.insertMany(req.body)
-
-        res.send({})
-
-    })
-
-    app.get('/', async (req, res) => {
-        const languagesAndVotes = (await votesCollection.find().toArray())
-            .map(({name, votes}) => new LanguageVote(name, votes))
-        console.log(languagesAndVotes)
-        res.send({
-            languages: languagesAndVotes
-        })
-    })
-
-    app.put('/vote/:langName', async (req, res) => {
-        const languagename = req.params['langName']
-        await votesCollection.updateOne({name: languagename}, {$inc: {votes: 1}})
-            res.send({message: "a voté pour " + languagename})
-    })
-
-    return app
-
+export async function stopApp() {
+  await client.close();
 }
