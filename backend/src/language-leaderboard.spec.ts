eed26ba30b1deba0d8e8d9202198ef6e6d2afbdd
johan.martinsson <johan.martinsson@dexyos.fr>
2022-06-05 12:00:15 +0200

    finished tests and code

--- backend/src/language-leaderboard.spec.ts
+++ backend/src/language-leaderboard.spec.ts
@@ -1,7 +1,71 @@
+import {Express} from "express";
+import supertest from "supertest"
+import {configureApp, VoteResult} from "./language-leaderboard"
+import {MongoMemoryServer} from "mongodb-memory-server"
 
 describe('LanguageLeaderBoard', () => {
 
+    let app: Express
+    beforeEach(async () => {
+        app = await configureApp()
+        await supertest(app).put('/reset').expect(200)
+    });
+
+    describe('/votes/:language - to vote for any language', () => {
+
+        it('haskell', async () => {
+            const result = await voteFor('haskell')
+            expect(result).toEqual(new VoteResult('haskell', 1))
+        });
+
+        it('rust', async () => {
+            const result = await voteFor('rust')
+            expect(result).toEqual(new VoteResult('rust', 1))
+        });
+
+        it('accumulates votes', async () => {
+            await voteFor('rust')
+            const secondVotesResult = await voteFor('rust')
+            expect(secondVotesResult).toEqual(new VoteResult('rust', 2))
+        });
+    });
+
+    describe('/votes - lists existing votes', () => {
+        it('when there are no votes', async () => {
+            const result = await getAllVotes()
+            expect(result).toEqual([])
+
+        });
+
+        it('with a single vote', async () => {
+            await voteFor('java')
+            const votes = await getAllVotes()
+            expect(votes).toEqual([new VoteResult('java', 1)])
+        });
+
+        it('with several votes for several languages', async () => {
+            await voteFor('java')
+            await voteFor('java')
+            await voteFor('java')
+            await voteFor('java')
+            await voteFor('js')
+            await voteFor('js')
+            const votes = await getAllVotes()
+            expect(votes).toContainEqual(new VoteResult('java', 4))
+            expect(votes).toContainEqual(new VoteResult('js', 2))
+        });
+
+    });
 
 
+    async function voteFor(language: string) {
+        const result = await supertest(app).put('/votes/' + language).expect(200)
+        return result.body
+    }
+
+    async function getAllVotes() {
+        const result = await supertest(app).get('/votes').expect(200)
+        return result.body
+    }
 
 });
---
+++ backend/src/language-leaderboard.ts
@@ -0,0 +1,48 @@
+import express, {json} from "express"
+import {MongoMemoryServer} from "mongodb-memory-server"
+import * as mongoDB from "mongodb";
+
+export async function configureApp() {
+    const app = express()
+
+    const mongod = await MongoMemoryServer.create()
+
+    const client = new mongoDB.MongoClient(mongod.getUri())
+    await client.connect()
+    const db = client.db('real-world-tdd')
+    const votesCollection = db.collection('votes')
+
+
+    app.put('/reset', async (req, res) => {
+        try {
+            await votesCollection.drop()
+        } catch (e){
+            console.warn(e)
+        }
+        res.send()
+
+    })
+    app.get('/votes', async (req, res) => {
+        const entries = await votesCollection.find().toArray()
+        const votes = entries.map((e) => new VoteResult(e.name, e.votes))
+        res.send(votes)
+    })
+
+    app.put('/votes/:language', async (req, res) => {
+        const language = req.params['language'] //?
+
+        const updateresult = await votesCollection.updateOne({name: language}, {$inc: {votes: 1}}, {upsert: true}) //?
+        const result = await votesCollection.findOne({name: language})
+
+        res.send(new VoteResult(result?.name, result?.votes))
+    })
+
+    return app
+}
+
+export class VoteResult {
+    constructor(public readonly language: string, public readonly votes: number) {
+
+    }
+
+}
