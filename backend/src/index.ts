5df373969d98eb6e1630fefc765d32754a265cdd
johan.martinsson <johan.martinsson@dexyos.fr>
2022-06-07 22:09:16 +0200

    fresh staring point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,22 @@
 // import { startApp } from "./language-leaderboard";
-import { MongoMemoryServer } from "mongodb-memory-server";
-import { configureApp } from "./leaderboard";
-
-async function start() {
-  const mongod = await MongoMemoryServer.create();
-  const app = await configureApp(mongod.getUri());
-  app.listen("3000");
-
-  console.log(`
-        go to http://localhost:3000
-        
-        to vote for cobol:          
-        PUT http://localhost:3000/vote/cobol
-        
-        to get all votes: 
-        GET http://localhost:3000/votes
-        
-        `);
-}
-
-start().catch((e) => console.error(e));
+// import { MongoMemoryServer } from "mongodb-memory-server";
+// import { configureApp } from "./leaderboard";
+//
+// async function start() {
+//   const mongod = await MongoMemoryServer.create();
+//   const app = await configureApp(mongod.getUri());
+//   app.listen("3000");
+//
+//   console.log(`
+//         go to http://localhost:3000
+//
+//         to vote for cobol:
+//         PUT http://localhost:3000/vote/cobol
+//
+//         to get all votes:
+//         GET http://localhost:3000/votes
+//
+//         `);
+// }
+//
+// start().catch((e) => console.error(e));
